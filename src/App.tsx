755a704f8d41dcb9914bf6d3a2b26aa645379af9
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-07-29 16:49:28 -0400

    add SearchBox component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import './App.css'
+import SearchBox from './components/SearchBox'
 
 function App() {
 
   return (
+    <>
    <div>App</div>
-  )
+   <SearchBox/>
+   </>
+   )
 }
 
 export default App
---
+++ src/components/SearchBox.tsx
@@ -0,0 +1,14 @@
+const SearchBox = () => {
+  return (
+    <div>
+      Serarch Box
+      <header>
+        <input type="text" />
+      </header>
+      <main>
+        <div>List Of Users</div>
+      </main>
+    </div>
+  );
+};
+export default SearchBox;
