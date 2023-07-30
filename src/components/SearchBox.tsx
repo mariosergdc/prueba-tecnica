561474b061a70d6282f8bc5488908ee3615ef3cf
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-07-29 22:45:55 -0400

    get and show users

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,4 +1,15 @@
+import { useState, useEffect } from "react";
 const SearchBox = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=100")
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.results);
+        setUsers(data.results);
+      });
+  }, []);
+
   return (
     <div>
       Serarch Box
@@ -7,6 +18,13 @@ const SearchBox = () => {
       </header>
       <main>
         <div>List Of Users</div>
+        {users.map((u) => {
+          return (
+            <div key={u.login.uuid}>
+              {u.name.first} {u.name.last}
+            </div>
+          );
+        })}
       </main>
     </div>
   );
