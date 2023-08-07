265ce082a9123e56e92695ff24e786b88c5942c5
MarioSergDC <mariosergdc.webdev@gmail.com>
2023-08-07 15:15:59 -0400

    save list in localStorage

--- src/components/PruebaTecinica2.tsx
+++ src/components/PruebaTecinica2.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import db from "../db/db.json";
 import BooksList from "./BooksList";
 import ReadingList from "./ReadingList";
@@ -9,6 +9,24 @@ const PruebaTecinica2 = () => {
   const [books, setBooks] = useState(initialState);
   const [booksToRead, setBooksToRead] = useState([]);
   const [genreFilter, setGenreFilter] = useState("");
+
+  useEffect(() => {
+    const item = localStorage.getItem("booksToRead");
+    if (item) {
+      const readingList = JSON.parse(item);
+      setBooksToRead(readingList);
+
+      const readingListId = readingList.map((el) => el.book.ISBN);
+
+      const booksActual = [...books].filter(
+        (el) => !readingListId.includes(el.book.ISBN)
+      );
+
+      setBooks(booksActual);
+    } else {
+      setBooksToRead([]);
+    }
+  }, []);
 
   const addToReadingList = (id) => {
     const bookToAdd = books.find((el) => {
@@ -21,6 +39,7 @@ const PruebaTecinica2 = () => {
     newReadingList.push(bookToAdd);
     setBooksToRead(newReadingList);
     setBooks(newBooks);
+    localStorage.setItem("booksToRead", JSON.stringify(newReadingList));
   };
 
   const removeFromReadingList = (id) => {
@@ -34,6 +53,7 @@ const PruebaTecinica2 = () => {
       return el.book.ISBN !== id;
     });
     setBooksToRead(bookFiltred);
+    localStorage.setItem("booksToRead", JSON.stringify(bookFiltred));
   };
 
   const handleGenreFilter = (e) => {
